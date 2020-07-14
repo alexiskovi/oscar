c9e6fb0d1f2f4be9fccc164a6f2fa7a43ac7b218
Коваленок Александр <kovalenok.av@starline.ru>
2020-07-14 19:33:09 +0300

    Docker: added volumes for sound card

--- docker/scripts/dev_start.sh
+++ docker/scripts/dev_start.sh
@@ -375,6 +375,11 @@ function main(){
         --pid=host \
         -v /dev/null:/dev/raw1394 \
         -v /pool/logs/data:/apollo/data \
+        --device /dev/snd \
+        -e PULSE_SERVER=unix:${XDG_RUNTIME_DIR}/pulse/native \
+        -v ${XDG_RUNTIME_DIR}/pulse/native:${XDG_RUNTIME_DIR}/pulse/native \
+        -v ~/.config/pulse/cookie:/root/.config/pulse/cookie \
+        --group-add $(getent group audio | cut -d: -f3) \
         $IMG \
         /bin/bash
     if [ $? -ne 0 ];then
